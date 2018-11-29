a491d7d19e596b332b23633dfc22472709c78dc9
Amanda <mndcheng@github.com>
2018-11-28 22:57:48 -0800

    calls stringC in interp

--- p6.fs
+++ p6.fs
@@ -2,6 +2,7 @@ include objects.fs
 
 variable numVal
 variable boolVal
+variable stringVal
 
 object class \ "object" is the parent class
    selector objValue ( -- )
@@ -61,29 +62,36 @@ exprC class
    overrides objType
 
    :noname ( str stringC -- )
-      val ! ;
+      val ! 
+      val stringVal ! ;
    overrides construct
 
 end-class stringC
 
 
 : interp ( exprC -- value )
-   dup 1 = if                        numVal ?   else
-   dup 2 = boolVal 0 = and if        ." false"  else
-   dup 2 = boolVal 0 = invert and if ." true"   else
+   dup 1 = if                        numVal ?      else
+   dup 2 = boolVal 0 = and if        ." false"     else
+   dup 2 = boolVal 0 = invert and if ." true"      else
+   dup 3 = if                        stringVal @ .s  else
                                      ." error"
-   then then then drop ; 
+   then then then then drop ; 
 
 
 24 numC heap-new constant my-num \ new numC
 -1 boolC heap-new constant my-bool \ new boolC 
+s" i" stringC heap-new constant my-string \ new stringC
 
 \ calling interp on numC 
 cr
-my-num typ interp 
+my-num objType interp 
 
 \ calling interp on boolC 
 cr
-my-bool typ interp
+my-bool objType interp
+
+\ calling interp on stringC 
+cr
+my-string objType interp
 
 cr
